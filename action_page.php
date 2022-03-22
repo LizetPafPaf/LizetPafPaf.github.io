4370b63d86ed36a8a115ffc8d091173ae2189d4b
LIZET-GONZALEZ-LOPEZ <99769712+LIZET-GONZALEZ-LOPEZ@users.noreply.github.com>
2022-03-22 17:16:41 -0600

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+  Your Apellidos is: <?php echo $_POST["lname"]; ?>
+  Your Direccion is: <?php echo $_POST["numero"]; ?>
+  Your Deporte is: <?php echo $_POST["deporte"]; ?>
+
+</body>
+</html>
